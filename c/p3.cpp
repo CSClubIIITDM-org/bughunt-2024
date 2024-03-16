94df8c3544b68bdc4b29319ce0078f4f2481a471
Shalini <124187336+Shalu42@users.noreply.github.com>
2024-03-16 11:52:31 +0530

    Update p3.cpp

--- c/p3.cpp
+++ c/p3.cpp
@@ -1,6 +1,3 @@
-//program return an array which contains product of all elements in a given array except the element at that index
-// eg : [1,2,3,4] -> [24,12,8,6]
-
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -8,23 +5,23 @@ using namespace std;
 vector<int> productExceptSelf(vector<int>& nums) {
     vector<int> ans(nums.size());
     ans[0] = 1;
-    for(int i=1;i<nums.size();i++){
-        ans[i] = ans[i-1] * nums[i-1];
+    for(int i = 1; i < nums.size(); i++) {
+        ans[i] = ans[i - 1] * nums[i - 1];
     }
     int r = 1;
-    for(int i=nums.size()-1;i>=0;i--){
+    for(int i = nums.size() - 2; i >= 0; i--) { // Corrected loop starting index
+        r *= nums[i + 1]; // Update r before multiplication
         ans[i] *= r;
-        r = nums[i];
     }
     return ans;
 }
 
-int main(){
-    vector<int> nums = {1,2,3,4};
+int main() {
+    vector<int> nums = {1, 2, 3, 4};
     vector<int> ans = productExceptSelf(nums);
-    for(int i=0;i<ans.size();i++){
-        cout<<ans[i]<<" ";
+    for(int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
     return 0;
 }
