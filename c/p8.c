dc78ddff7211aad0dd272633e33a5426c673df08
Sahil <118194056+Sahilll94@users.noreply.github.com>
2024-03-16 10:23:56 +0530

    Update p8.c
    We made the changes in function computeAverage that takes the array x and its size N directly as arguments.

--- c/p8.c
+++ c/p8.c
@@ -2,20 +2,17 @@
 
 #include <stdio.h>
 
-float computeAverage(float* x, size_t* N) {
-    float numerator = 0.0;
-    float denominator = 0.0;
-    for (size_t i = 0; i < (size_t)N; i++) {
-        numerator += x[i];
-        denominator++;
+float computeAverage(float* x, size_t N) {
+    float sum = 0.0;
+    for (size_t i = 0; i < N; i++) {
+        sum += x[i];
     }
-    return numerator/denominator;
+    return sum / N;
 }
 
-int main() 
-{
+int main() {
     float x[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     size_t N = 10;
-    printf("%.3g\n", computeAverage(x, &N));
+    printf("%.3g\n", computeAverage(x, N));
     return 0;
 }
