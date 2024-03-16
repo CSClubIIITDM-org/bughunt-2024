fb8d94f66a4b9381937f2c4d55905d5c0a1d48fc
Vignesh Aravindh B CS22B2004 <118040393+codechiefVignesh@users.noreply.github.com>
2024-03-16 11:58:58 +0530

    Update p2.py

--- py/p2.py
+++ py/p2.py
@@ -1,9 +1,10 @@
 #program to print an input as the only member of an array everytime the function is called
 # input- 1 -> output- [1], input- 2 -> output- [2]
-
-def add_item(item, items=[]):
+items = []
+def add_item(item):
    items.append(item)
    return items
 
 print(add_item(1))
+items.clear()
 print(add_item(2))
